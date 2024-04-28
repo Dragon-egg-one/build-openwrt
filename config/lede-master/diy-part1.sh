e1a3ef462928b53c457b01691e884aa9a3d67651
longdan <58099612+Dragon-egg-one@users.noreply.github.com>
2024-04-28 20:49:02 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,7 +8,10 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git kiddin https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >> feeds.conf.default
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
