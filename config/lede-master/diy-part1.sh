628820bb113ebd1dc9b174f4e56569e8b2334486
longdan <58099612+Dragon-egg-one@users.noreply.github.com>
2024-05-04 10:06:40 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -13,17 +13,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 删除自定义源默认的 argon 主题
-rm -rf package/lean/luci-trgheme-aon
 
-# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
-find ./ -name luci-theme-argon | xargs rm -rf;
-
-# 针对 LEDE 项目拉取 argon 原作者的源码
-git clone -b 18.06 http pacs://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# 替换默认主题为 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
